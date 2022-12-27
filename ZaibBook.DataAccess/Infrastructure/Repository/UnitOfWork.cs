640c875bb9c262bcaa4c205fac4c739070abc589
shahzaib <m.shahzaib.123456@gmail.com>
2022-12-27 14:00:21 +0500

    Unit of Work Pattren Created

---
+++ ZaibBook.DataAccess/Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZaibBook.DataAccess.Infrastructure.IRepository;
+
+namespace ZaibBook.DataAccess.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public ICategoryRepository Category { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            Category = new CategoryRepository(context);
+        }
+        
+
+        public void Save()
+        {
+           _context.SaveChanges();
+        }
+    }
+}
