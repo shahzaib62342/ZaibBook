63ffdef15832d51cc863bacb564fbafa4929628b
shahzaib <m.shahzaib.123456@gmail.com>
2022-12-27 11:58:03 +0500

    Generic Interface IRepository for common functions to use IRepository Pattren

--- ZaibBook.DataAccess/Infrastructure/IRepository/IRepository.cs
+++ ZaibBook.DataAccess/Infrastructure/IRepository/IRepository.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ZaibBook.DataAccess.Infrastructure.IRepository
 {
-    //Generic Interface
+    //Generic Interface for common functions
     public interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+
+        T GetT(Expression<Func<T, bool>> predicate);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void DeleteRange(IEnumerable<T> entities);
+
     }
 }
