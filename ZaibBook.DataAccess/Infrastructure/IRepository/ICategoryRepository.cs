5489cd9bd799f665eae52e3af2e94c76a825e805
shahzaib <m.shahzaib.123456@gmail.com>
2022-12-27 13:40:50 +0500

    Repository Pattren implemented

---
+++ ZaibBook.DataAccess/Infrastructure/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZaibBook.DataAccess.Infrastructure.Repository;
+using ZaibBook.Models;
+
+namespace ZaibBook.DataAccess.Infrastructure.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
