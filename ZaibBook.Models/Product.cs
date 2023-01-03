d31aa08299d251d58ba0935fa935da81c815cc76
shahzaib <m.shahzaib.123456@gmail.com>
2023-01-03 14:21:52 +0500

    Added new Product Model Class

---
+++ ZaibBook.Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaibBook.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Use Alphabets only")]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
