e80562483acd626877a55d3bb47db91640b86e96
shahzaib <m.shahzaib.123456@gmail.com>
2023-01-03 17:22:41 +0500

    Added IEnumirable Category Property in CategoryMV for GetList

--- ZaibBook.Models/ViewModel/CategoryVM.cs
+++ ZaibBook.Models/ViewModel/CategoryVM.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ZaibBook.Models.ViewModel
+﻿namespace ZaibBook.Models.ViewModel
 {
     public class CategoryVM
     {
         public Category Category { get; set; }
+        public IEnumerable<Category> categories { get; set; }=new List<Category>();
     }
 }
