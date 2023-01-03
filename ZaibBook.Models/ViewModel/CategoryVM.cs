b78daa49b08f35f8c112ab3ee03f18be46036fc6
shahzaib <m.shahzaib.123456@gmail.com>
2023-01-03 17:38:45 +0500

    Initalize Category to Resolve error while addding new category

--- ZaibBook.Models/ViewModel/CategoryVM.cs
+++ ZaibBook.Models/ViewModel/CategoryVM.cs
@@ -2,7 +2,7 @@
 {
     public class CategoryVM
     {
-        public Category Category { get; set; }
+        public Category Category { get; set; } = new Category();
         public IEnumerable<Category> categories { get; set; }=new List<Category>();
     }
 }
